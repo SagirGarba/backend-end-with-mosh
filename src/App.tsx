6b3d3090586665962a710435395216c7ccb0d27b
SagirGarba <sagir40me@gmail.com>
2023-10-07 15:57:04 +0100

    Worked on Axios, using promise (then, and catch)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import { useEffect, useRef, useState } from "react";
-import ProductList from "./ProductList";
+import axios, {AxiosError} from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
+// import { useEffect, useRef, useState } from "react";
+// import ProductList from "./ProductList";
+
+// const connect = () => console.log("Connecting");
+// const disconnect = () => console.log("Disconnecting");
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
-  const ref = useRef<HTMLInputElement>(null);
-
-  const [category, setCategory] = useState("");
-
-  useEffect(() => {
-    document.title = "Back End";
-  });
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    connect();
+    //Using await and async
+    const fetchUsers = async () => {
+      const res = await axios.get<User[]>(
+        "https://jsonplaceholder.typicode.com/users"
+        
+      );
+      setUsers(res.data)
+    };
+    catch (error) {
+      setError((err as AxiosError).message);
+    }
 
-    return () => disconnect();
-  });
+    //Using promise to get data with then as response and catch to catch error
+    // axios
+    //   .get<User[]>("https://jsonplaceholder.typicode.com/Xusers")
+    //   .then((res) => setUsers(res.data))
+    //   .catch((err) => {
+    //     setError(err.message);
+    //   });
+  }, []);
+  // const ref = useRef<HTMLInputElement>(null);
+
+  // const [category, setCategory] = useState("");
+
+  // useEffect(() => {
+  //   document.title = "Back End";
+  // });
+
+  // useEffect(() => {
+  //   connect();
+
+  //   return () => disconnect();
+  // });
 
   return (
     <div>
-      <div className="mb-5"></div>
+      {error && <h1 className="text-danger">{error}</h1>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      {/* <div className="mb-5"></div>
       <select
         name=""
         id=""
@@ -31,10 +70,10 @@ const App = () => {
         <option value=""></option>
         <option value="clothing">Clothing</option>
         <option value="household">Household</option>
-      </select>
+      </select> */}
       {/* <input ref={ref} type="text" className="form-control" /> */}
 
-      <ProductList category={category} />
+      {/* <ProductList category={category} /> */}
     </div>
   );
 };
